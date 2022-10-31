5dae68a9be077148227397b956e4c2f08c1f8f57
John <jotesto@gmail.com>
2022-10-31 10:45:30 +0200

    some fitches

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import './assets/style.scss';
 import App from './App.vue';
 import routes from './router/router';
-import {createPinia} from 'pinia';
+import { createPinia } from 'pinia';
 const pinia = createPinia();
 
 createApp(App).use(pinia).use(routes).mount('#app');
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://app-heromangment.herokuapp.com/hero/',
+  baseURL: import.meta.env.VITE_URL,
 });
 
 export default api;
